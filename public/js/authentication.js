ab9f06100c2e2668cba43438c487cae68cac5f3b
kennentran <dainghia.tran171@gmail.com>
2020-11-14 17:41:48 +0700

    Slide builder completed

--- public/js/authentication.js
+++ public/js/authentication.js
@@ -21,11 +21,6 @@ $(document).ready(function () {
     $("#student-tab").click();
 });
 
-
-$("#teacherlogin").click(() => {
-    $("#login-tab").click();
-});
-
 $("#signup").click(() => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     console.log(re.test(String($("#email").val()).toLocaleLowerCase()));
